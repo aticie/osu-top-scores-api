cbf3310af1535a3359941ed37f7e854db35993fe
Sibyl <metinkorkmaz417@gmail.com>
2022-07-30 18:34:20 +0300

    better mobile support

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -14,46 +14,54 @@
   let includeHD = false;
   let ppRange = [500, 800];
   //
+
+  let page = 1;
+  let beatmaps = [];
 </script>
 
 <main class="flex flex-col gap-4 w-full max-w-2xl">
   <p class="font-semibold text-2xl text-center">osu! Top 1000 Scores Database</p>
 
-  <div class="grid grid-cols-2 gap-x-16 gap-y-6 items-center">
-    <div class="flex flex-col gap-2">
-      <p class="setting-title">Mods</p>
-      <div class="flex gap-2">
-        {#each allMods as mod}
-          <div class="relative flex items-center justify-center">
-            <input
-              class="appearance-none rounded-full border-2 border-neutral-700 h-12 w-12 checked:border-red-primary transition-colors"
-              type="radio"
-              bind:group={currentMods}
-              name="mods"
-              id={mod.text}
-              value={mod.req}
-            />
-            <label class="absolute select-none" for={mod.text}>{mod.text}</label>
-          </div>
-        {/each}
+  <div class="flex flex-col gap-4 ">
+    <div class="flex flex-col md:flex-row items-center justify-around gap-4">
+      <div class="flex flex-col gap-2">
+        <p class="setting-title">Mods</p>
+        <div class="flex gap-2">
+          {#each allMods as mod}
+            <div class="relative flex items-center justify-center">
+              <input
+                class="appearance-none rounded-full border-2 border-neutral-700 h-12 w-12 checked:border-red-primary transition-colors"
+                type="radio"
+                bind:group={currentMods}
+                name="mods"
+                id={mod.text}
+                value={mod.req}
+              />
+              <label class="absolute select-none" for={mod.text}>{mod.text}</label>
+            </div>
+          {/each}
+        </div>
+      </div>
+  
+      <div class="flex flex-col gap-2">
+        <div class="checkbox-container">
+          <input class="checkbox" type="checkbox" id="unicode" bind:checked={unicode} />
+          <label class="checkbox-label" for="unicode">Unicode Titles</label>
+        </div>
+        <div class="checkbox-container">
+          <input class="checkbox" type="checkbox" id="includeHD" bind:checked={includeHD} />
+          <label class="checkbox-label" for="includeHD">Include HD</label>
+        </div>
       </div>
     </div>
 
-    <div class="flex flex-col gap-2">
-      <div class="checkbox-container">
-        <input class="checkbox" type="checkbox" id="unicode" bind:checked={unicode} />
-        <label class="checkbox-label" for="unicode">Unicode Titles</label>
-      </div>
-      <div class="checkbox-container">
-        <input class="checkbox" type="checkbox" id="includeHD" bind:checked={includeHD} />
-        <label class="checkbox-label" for="includeHD">Include HD</label>
-      </div>
-    </div>
 
-    <div class="col-span-2">
+    <div>
       <p class="setting-title">PP Range</p>
       <RangeSlider range float pushy step={5} min={400} max={1300} bind:values={ppRange} />
     </div>
+
+    <button class="bg-red-primary rounded-md p-2 hover:bg-neutral-800 transition-colors col-span-2">Get Beatmaps</button>
   </div>
 </main>
 
@@ -61,7 +69,6 @@
     let beatmaps = [];
     let pp_range = [500, 800]
     let page = 1;
-
 
     const infiniteHandler = ({detail: {loaded, complete}}) => {
         axios
@@ -90,33 +97,6 @@
 </script>
 
 <main>
-    <div>
-        <h1>osu! Top 1000 Scores Database</h1>
-    </div>
-
-    <div class="settings">
-        <div class="mods">
-            {#each all_mods as mod}
-                <div class="btn-mods">
-                    <input type=radio bind:group={mods} name="mods" id={mod.text} value={mod.req}>
-                    <label for={mod.text}>{mod.text}</label>
-                </div>
-            {/each}
-        </div>
-        <div class="checkboxes">
-            <div class="checkbox-single">
-                <input class="checkbox" type="checkbox" bind:checked={unicode}/> Unicode Titles
-            </div>
-            <div class="checkbox-single">
-                <input class="checkbox" type="checkbox" bind:checked={include_hd}/> Include HD
-            </div>
-        </div>
-        <Button
-                text="Get Beatmaps"
-                onClick={submit}
-        />
-        <Slider on:message={setSliderValue}/>
-    </div>
     <div class="beatmaps">
         {#each beatmaps as bmap}
             <div class="beatmap-single">
