4879a588a72a47bc471ed5bb8ce74cf43a23fd85
Sibyl <metinkorkmaz417@gmail.com>
2022-07-30 16:33:59 +0300

    add tailwindcss, switch functions to arrow functions

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,7 +4,6 @@
     import Button from './lib/Button.svelte';
     import Slider from "./lib/Slider.svelte";
 
-
     const all_mods = [
         {text: "Any", req: "any"},
         {text: "NM", req: ""},
@@ -12,7 +11,6 @@
         {text: "DT", req: "dt"},
     ];
 
-    let top_n = 5;
     let mods = 'any';
     let unicode = false;
     let include_hd = true;
@@ -20,14 +18,13 @@
     let pp_range = [500, 800]
     let page = 1;
 
-
-    function setSliderValue(event) {
+    const setSliderValue = (event) => {
         console.log('New pp range: ' + pp_range)
         console.log(event)
         pp_range = event.detail
     }
 
-    function infiniteHandler({detail: {loaded, complete}}) {
+    const infiniteHandler = ({detail: {loaded, complete}}) => {
         axios
             .get('/beatmaps', {params: {mod: mods, pp_range: pp_range, include_hd: include_hd, page: page}})
             .then((res) => {
@@ -41,7 +38,7 @@
             });
     }
 
-    let submit = () => {
+    const submit = () => {
         beatmaps = [];
         page = 1;
         axios
@@ -54,9 +51,10 @@
 </script>
 
 <main>
-    <div class="title">
+    <div>
         <h1>osu! Top 1000 Scores Database</h1>
     </div>
+
     <div class="settings">
         <div class="mods">
             {#each all_mods as mod}
@@ -120,160 +118,3 @@
     <InfiniteLoading on:infinite={infiniteHandler}/>
 </main>
 
-<style>
-    :root {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        background: #171717;
-    }
-
-    .settings, .mods {
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: center;
-    }
-
-    .settings {
-        padding-top: 0.5em;
-        position: sticky;
-        top: 0;
-        background: #171717;
-    }
-
-    .checkbox {
-        appearance: none;
-        width: 20px;
-        height: 20px;
-        background-color: white;
-        border-radius: 0.375rem;
-        background-image: url("../tick.svg");
-        transition: all 150ms ease;
-    }
-
-    .checkbox:checked {
-        background-color: #da0037;
-    }
-
-
-    .btn-mods label {
-        display: inline-block;
-        width: 2em;
-        padding: 0.3em;
-        border: solid 2px #ccc;
-        border-radius: 0.5em;
-        transition: all 0.3s;
-        color: #ffffff;
-        margin: 0.5em;
-    }
-
-    .btn-mods input[type="radio"] {
-        display: none;
-    }
-
-    .btn-mods input[type="radio"]:checked + label {
-        border: solid 2px #da0037;
-    }
-
-    .checkboxes {
-        display: flex;
-        color: #EDEDED;
-        align-items: center;
-    }
-
-    .checkbox-single {
-        margin: 0 1em;
-        display: flex;
-        align-items: center;
-        gap: 5px;
-    }
-
-    .beatmaps {
-        margin-top: 3rem;
-        display: flex;
-        flex-direction: column;
-        color: #da0037;
-    }
-
-    .beatmap-single {
-        font-size: x-large;
-        display: flex;
-        margin: 0.5rem 0;
-        justify-content: center;
-    }
-
-    .beatmap-url {
-        display: flex;
-        flex-grow: 1;
-        text-decoration: none;
-        color: #fff;
-        max-width: 900px;
-    }
-
-    .beatmap-cover {
-        display: flex;
-        min-height: 5rem;
-        padding: 1rem;
-        flex-grow: 1;
-        border-radius: 2rem;
-        text-align: center;
-        flex-direction: column;
-        justify-content: center;
-        background-size: cover;
-        background-position: center;
-    }
-
-    .beatmap-details {
-        display: flex;
-        flex-direction: column;
-        margin-left: 1em;
-        color: #EDEDED;
-    }
-
-    .beatmap-detail {
-        margin: 2em auto;
-    }
-
-    .detail-title {
-        font-weight: bold;
-    }
-
-    input {
-        color: #171717;
-    }
-
-    main {
-        text-align: center;
-        padding: 1rem;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #da0037;
-        font-size: 2rem;
-        font-weight: 350;
-        line-height: 1.1;
-        margin: 0.5rem auto;
-    }
-
-    p {
-        margin: 1rem auto;
-        line-height: 1.35;
-        color: #EDEDED;
-    }
-
-    span {
-        font-weight: bold;
-    }
-
-    @media (max-width: 576px) {
-        .beatmap-single {
-            flex-direction: column;
-        }
-
-        .beatmap-details {
-            margin-left: 0;
-            flex-direction: row;
-        }
-
-    }
-</style>
