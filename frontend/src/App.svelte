85d2ce2d20af33e6a8fbc9c18fcd607abd847ef7
Sibyl <metinkorkmaz417@gmail.com>
2022-07-30 19:18:26 +0300

    enough rework I guess

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,7 @@
 <script>
+  import axios from "axios";
   import RangeSlider from "svelte-range-slider-pips";
+  import InfiniteLoading from "svelte-infinite-loading";
 
   const allMods = [
     { text: "NM", req: "" },
@@ -9,7 +11,7 @@
   ];
 
   // settings
-  let currentMods = "any";
+  let mod = "any";
   let unicode = false;
   let includeHD = false;
   let ppRange = [500, 800];
@@ -17,6 +19,31 @@
 
   let page = 1;
   let beatmaps = [];
+
+  const getBeatmaps = async () => {
+    const response = await axios.get("/beatmaps", {
+      params: {
+        mod,
+        page,
+        pp_range: ppRange,
+        include_hd: includeHD,
+      },
+    });
+
+    beatmaps = [...beatmaps, ...response.data.beatmaps];
+    page++;
+
+    return beatmaps.length;
+  };
+
+  const loadBeatmaps = async ({ detail: { loaded, complete } }) => {
+    let length = await getBeatmaps();
+    if (length) {
+      loaded();
+    } else {
+      complete();
+    }
+  };
 </script>
 
 <main class="flex flex-col gap-4 w-full max-w-2xl">
@@ -27,22 +54,22 @@
       <div class="flex flex-col gap-2">
         <p class="setting-title">Mods</p>
         <div class="flex gap-2">
-          {#each allMods as mod}
+          {#each allMods as mods}
             <div class="relative flex items-center justify-center">
               <input
                 class="appearance-none rounded-full border-2 border-neutral-700 h-12 w-12 checked:border-red-primary transition-colors"
                 type="radio"
-                bind:group={currentMods}
+                bind:group={mod}
                 name="mods"
-                id={mod.text}
-                value={mod.req}
+                id={mods.text}
+                value={mods.req}
               />
-              <label class="absolute select-none" for={mod.text}>{mod.text}</label>
+              <label class="absolute select-none" for={mods.text}>{mods.text}</label>
             </div>
           {/each}
         </div>
       </div>
-  
+
       <div class="flex flex-col gap-2">
         <div class="checkbox-container">
           <input class="checkbox" type="checkbox" id="unicode" bind:checked={unicode} />
@@ -55,85 +82,47 @@
       </div>
     </div>
 
-
     <div>
       <p class="setting-title">PP Range</p>
       <RangeSlider range float pushy step={5} min={400} max={1300} bind:values={ppRange} />
     </div>
 
-    <button class="bg-red-primary rounded-md p-2 hover:bg-neutral-800 transition-colors col-span-2">Get Beatmaps</button>
+    <button
+      on:click={getBeatmaps}
+      class="bg-red-primary rounded-md p-2 hover:bg-neutral-800 transition-colors col-span-2">Get Beatmaps</button
+    >
   </div>
-</main>
 
-<!-- <script>
-    let beatmaps = [];
-    let pp_range = [500, 800]
-    let page = 1;
-
-    const infiniteHandler = ({detail: {loaded, complete}}) => {
-        axios
-            .get('/beatmaps', {params: {mod: mods, pp_range: pp_range, include_hd: include_hd, page: page}})
-            .then((res) => {
-                if (res.data.beatmaps.length) {
-                    beatmaps = [...beatmaps, ...res.data.beatmaps];
-                    page++;
-                    loaded();
-                } else {
-                    complete();
-                }
-            });
-    }
-
-    const submit = () => {
-        beatmaps = [];
-        page = 1;
-        axios
-            .get('/beatmaps', {params: {mod: mods, pp_range: pp_range, include_hd: include_hd, page: page}})
-            .then((res) => {
-                beatmaps = [...beatmaps, ...res.data.beatmaps];
-                page++;
-            });
-    };
-</script>
-
-<main>
-    <div class="beatmaps">
-        {#each beatmaps as bmap}
-            <div class="beatmap-single">
-                <a class="beatmap-url" href="https://osu.ppy.sh/b/{bmap.beatmap_id}">
-                    <div class="beatmap-cover"
-                         style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('{bmap.cover_url}')">
-                        <span>{#if unicode}{bmap.artist_unicode}
-                            {:else }{bmap.artist}{/if}</span>
-                        <br>
-                        <span>{#if unicode}{bmap.title_unicode}
-                            {:else }{bmap.title}{/if}</span>
-                        <p>{bmap.difficulty}</p>
-                    </div>
-                </a>
-                <div class="beatmap-details">
-                    <div class="beatmap-detail">
-                        <div class="detail-title">
-                            Average PP
-                        </div>
-                        <div class="detail-value">
-                            {(Math.round(bmap.avg_pp * 100) / 100).toFixed(2)}
-                        </div>
-                    </div>
-                    <div class="beatmap-detail">
-                        <div class="detail-title">
-                            Play Count
-                        </div>
-                        <div class="detail-value">
-                            {bmap.play_count}
-                        </div>
-                    </div>
-
-                </div>
+  <div>
+    {#each beatmaps as bmap}
+      <a href="https://osu.ppy.sh/b/${bmap.beatmap_id}" class="rounded-full overflow-hidden relative">
+        <div class="w-full h-32 rounded-md bg-cover" style="background-image: url({bmap.cover_url})" >
+          <div class="flex justify-between items-end absolute inset-3 z-10">
+            <div class="flex flex-col justify-between h-full">
+              <p>{bmap.artist}</p>
+              <div>
+                <p class="font-semibold">{ bmap.title }</p>
+                <p>{bmap.difficulty}</p>
+              </div>
             </div>
-        {/each}
-    </div>
 
-    <InfiniteLoading on:infinite={infiniteHandler}/>
+            <div class="flex gap-4">
+              <div class="text-center">
+                <p class="font-semibold">Average PP</p>
+                <p>{bmap.avg_pp.toFixed(2)}</p>
+              </div>
+              <div class="text-center">
+                <p class="font-semibold">Play Count</p>
+                <p>{bmap.play_count}</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="absolute inset-0 bg-black bg-opacity-70" />
+        </div>
+      </a>
+    {/each}
+  </div>
+
+  <InfiniteLoading on:infinite={loadBeatmaps} />
 </main>
- -->
